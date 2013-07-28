a85cd79eacbc43e9cf62cd53fb03d3f2ab707901
Javier Novoa <jstitch@gmail.com>
2013-07-27 22:25:55 -0500

    init commit

---
+++ norma.py
@@ -0,0 +1,219 @@
+#
+
+# No se pueden definir macros dentro de macros
+# No se pueden definir macros mas de una vez, ni con nombres reservados
+# Las macros no pueden usar macros que no hayan sido previamente definidas (y por ende no puede haber tampoco refs. circulares)
+# Una macro no puede llamarse a si misma
+# Espacio de memoria de DATOS (para variables locales) a partir de 0xF000 y hasta 0xFFFC (4093 direcciones)
+
+# Como NORMA solo funciona con NORs, la idea es tomar el archivo de entrada y traducir todo a NORs.
+# El traductor primero toma las macros y almacena su codigo y luego al recorrer el codigo en si mismo, lo traduce a NORs que pueden ser ejecutados por cpu()
+
+debug = False
+
+TAM_MEMORIA = 0xFFFF
+
+def nor(a, b):
+    return ~(a | b) & TAM_MEMORIA
+
+mem = [0] * (TAM_MEMORIA + 1)
+
+IP = 0xFFFF
+SR = 0xFFFE
+OUT = 0xFFFD
+
+DATA = 0xF000
+
+# array de palabras reservadas
+reserved = ["local", "set", "label", "macro", "endm"]
+# array de nombres de vars locales (strings)
+locales = []
+# array de nombres de labels (dicts con keys 'name' (nombre del label) y 'ip' (dir a poner a IP cuando se usen))
+labels = []
+# array de macros a expandir durante load (dicts con keys 'name' (nombre de la macro), 'args' (list de nombres de args) y 'body' (list de lineas de codigo))
+macros = []
+# array con variables locales (creo que sobra)
+data = []
+# valor de ip conforme se traduce el programa a memoria
+ip = 0
+
+# Convierte una palabra a:
+# - numero de direccion predefinida en memoria (IP, SR, OUT, DATA, ...)
+# - numero de direccion valida en memoria
+# - nombre de variable local (a valor en memoria de datos donde reside esa variable)
+# - nombre de label (a valor de IP apuntada por la label)
+def convierte(palabra):
+    global mem
+    
+    if palabra == "IP":
+        return IP
+    elif palabra == "SR":
+        return SR
+    elif palabra == "OUT":
+        return OUT
+    elif palabra == "DATA":
+        return DATA
+ 
+    try:
+        addr = int(palabra)
+        if addr < 0 or addr > ( len(mem) - 1 ):
+            raise Exception("Direccion de memoria invalida")
+    except (ValueError, IndexError) as direrr:
+        try:
+            addr = int(DATA + locales.index(palabra))
+            mem[addr]
+        except (ValueError, IndexError) as locerr:
+            try:
+                etiquetas = [ l['name'] for l in labels ]
+                addr = int(labels[etiquetas.index(palabra)]['ip'])
+                mem[addr]
+            except (ValueError, IndexError) as laberr:
+                raise Exception("La palabra '%s' no pudo ser convertida" % palabra)
+    return addr
+
+# Traduce una linea de codigo a lineas N1, N2, N3 donde Nn son direcciones de memoria
+# - puesto que encuentra llamadas a macros, llama recursivamente a esta funcion para conseguir N1,N2,N3
+def traduce(codigo):
+    global ip, mem
+
+    if codigo[0] == "local": # reservar memoria para variable y mapear nombre
+        for local in codigo[1:] :
+            locales.append(local)
+            data.append(0)
+    elif codigo[0] == "set": # asignar valor a variable mapeada o direccion
+        setstr = codigo[1:]
+        try:
+            setval = int(setstr[0])
+        except ValueError as verr:
+            setval = int(setstr[0], 16)
+        setdir = setstr[1] # la direccion puede ser una local ya declarada o una direccion en memoria a usar directamente
+        try:
+            data[locales.index(setdir)] = setval
+            mem[DATA + locales.index(setdir)] = setval
+        except ValueError as verr:
+            try:
+                mem[int(setdir)] = setval
+            except ValueError as verr:
+                raise Exception("'%s' no es una variable definida" % setdir)
+            except IndexError as ierr:
+                raise Exception("La direccion '%s' esta fuera de rango" % setdir)
+    elif codigo[0] == "label": # guardar direccion de memoria en memoria de labels
+        labels.append({'name':codigo[1], 'ip':ip})
+
+    elif codigo[0] in [ m['name'] for m in macros ]: # tomar funcion ya definida y guardar sus instrucciones en memoria tomando sus argumentos
+
+        macro = [ m for m in macros if m['name'] == codigo[0] ][0]
+        args = codigo[1:]
+        for linea in macro['body']:
+            line = linea[:]
+            for n,arg in enumerate(macro['args']):
+                for i,l in enumerate(line):
+                    if l == arg:
+                        line[i]=args[n]
+            try:
+                traduce(line)
+            except Exception as ex:
+                raise Exception("Macro %s: %s" % (macro['name'], ex))
+
+    else: # nor
+        mem[ip] = convierte(codigo[0])
+        mem[ip + 1] = convierte(codigo[1])
+        mem[ip + 2] = convierte(codigo[2])
+        ip += 3
+
+# Carga el archivo con el programa y recorre linea a linea, cargando macros y traduciendo codigo a NORs en memoria
+def loader(nomarchivo):
+    global mem
+    
+    archivo = open(nomarchivo)
+    lineas = archivo.readlines()
+    archivo.close()
+
+    inmacro = ""
+    for num, linea in enumerate(lineas):
+        p = [ e for e in linea.strip().split(";")[0].strip().replace(","," ").split(" ") if e is not "" ]
+        if not p:
+            continue
+
+        if debug: print "%s" % num, " ".join(p)
+        if p[0] == "macro": # leer lineas de macro y guardar como funcion definida
+            if inmacro:
+                raise Exception("No puedes definir una macro dentro de otra macro")
+            if p[1] in [ m['name'] for m in macros ]:
+                raise Exception("Macro '%s' ya estaba previamente definida" % p[1])
+            if p[1] in reserved:
+                raise Exception("El nombre '%s' para la macro es una palabra reservada" % p[1])
+
+            inmacro = p[1]
+            macro = {}
+            macro['name'] = p[1]
+            macro['args'] = []
+            try:
+                for arg in p[2:] :
+                    macro['args'].append(arg)
+            except IndexError as ierr:
+                pass
+            macro['body'] = []
+            macros.append(macro)
+        elif p[0] == "endm":
+            inmacro = ""
+            continue
+        elif inmacro:
+            if p[0] == macros[-1]['name']:
+                raise Exception("La macro %s se llama a si misma" % p[0])
+
+            macros[-1]['body'].append(p)
+
+        # Traduce linea para conseguir NORs en la memoria
+        if not inmacro:
+            traduce(p)
+
+    if inmacro:
+        raise Exception("No se encontro 'endm' para la macro %s" % inmacro)
+
+    # 'inicializa' el area de datos (probablemente no es necesario)
+    # for i,d in enumerate(data):
+    #     mem[DATA + i] = int(d)
+
+    return mem
+
+
+# Ejecuta lo que haya en memoria como NORs de 3 en 3 direcciones
+def cpu(archivo):
+    global mem
+    mem = loader(archivo)
+
+    mem[IP] = 0
+
+    while True:
+        i = mem[IP]
+        a = mem[mem[i+0]]
+        b = mem[mem[i+1]]
+        r = mem[i+2]
+        mem[IP] = i + 3
+        f = nor(a, b)
+        mem[r] = f
+        mem[SR] = ((f >> 15) & 1) | ((f & 0x7FFF) << 1)
+        if mem[IP] >= DATA:
+            break
+
+    print mem[OUT]
+
+
+# main
+if __name__ == "__main__":
+    import getopt, sys
+
+    archivo = "test.a" 
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "f:d:", ["file=","debug"])
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(2)
+    for o,a in sorted(opts):
+        if o in ("-f", "--file"):
+            archivo = a
+        elif o in ("-d", "--debug"):
+            debug = True
+
+    cpu(archivo)
---
+++ unit.py
@@ -0,0 +1,165 @@
+#!/usr/bin/pyrg
+# coding: utf-8
+
+import unittest
+
+import norma
+
+
+class TestConvierte(unittest.TestCase):
+    def test_01_dirpredefinida(self):
+        self.assertEqual(norma.convierte("IP"), norma.IP)
+        self.assertEqual(norma.convierte("SR"), norma.SR)
+        self.assertEqual(norma.convierte("OUT"), norma.OUT)
+        self.assertEqual(norma.convierte("DATA"), norma.DATA)
+
+    def test_02_dirnumerica(self):
+        self.assertEqual(norma.convierte("1000"), 1000)
+        self.assertRaisesRegexp(Exception, "Direccion de memoria invalida", norma.convierte, **{'palabra': str(norma.TAM_MEMORIA + 1)})
+        self.assertRaisesRegexp(Exception, "Direccion de memoria invalida", norma.convierte, **{'palabra': "-1"})
+
+    def test_03_dirlocal(self):
+        norma.locales.append("varlocal")
+        self.assertEqual(norma.convierte("varlocal"), norma.DATA)
+        norma.locales.append("varlocal2")
+        self.assertEqual(norma.convierte("varlocal2"), norma.DATA + 1)
+        norma.locales.append("varlocal3")
+        norma.locales.append("varlocal4")
+        norma.locales.append("varlocal5")
+        self.assertEqual(norma.convierte("varlocal5"), norma.DATA + 4)
+
+    def test_04_dirlabel(self):
+        norma.labels.append({'name': "label1", 'ip': 1001})
+        self.assertEqual(norma.convierte("label1"), 1001)
+        norma.labels.append({'name': "label2", 'ip': norma.IP})
+        self.assertEqual(norma.convierte("label2"), norma.IP)
+
+    def test_05_direrronea(self):
+        self.assertRaisesRegexp(Exception, "^La palabra '(\w+)' no pudo ser convertida$", norma.convierte, **{'palabra': "SPAM"})
+
+
+class TestTraduce(unittest.TestCase):
+    def setUp(self):
+        reload(norma)
+
+    def test_01_nor(self):
+        self.assertEqual(norma.ip, 0)
+        self.assertListEqual(norma.mem[0:3], [0, 0, 0])
+        norma.mem[1000] = 201
+        norma.mem[1001] = 202
+        norma.traduce(['1000','1001','1002'])
+        self.assertEqual(norma.ip, 3)
+        self.assertListEqual(norma.mem[0:3], [1000, 1001, 1002])
+
+    def test_02_local(self):
+        self.assertListEqual(norma.locales, [])
+        self.assertListEqual(norma.data, [])
+        norma.traduce(['local','t1','t2'])
+        self.assertListEqual(norma.locales, ['t1','t2'])
+        self.assertListEqual(norma.data, [0,0])
+        norma.mem[1000] = 300
+        norma.traduce(['1000','t1','t2'])
+        self.assertListEqual(norma.mem[0:3], [1000,norma.DATA,norma.DATA+1])
+
+    def test_03_set(self):
+        self.assertEqual(norma.mem[1001],0)
+        norma.traduce(['set',200,1001])
+        self.assertEqual(norma.mem[1001],200)
+        norma.traduce(['local','var'])
+        self.assertEqual(norma.data[0],0)
+        norma.traduce(['set',200,'var'])
+        self.assertEqual(norma.data[0],200)
+        self.assertEqual(norma.mem[norma.DATA], 200)
+        self.assertRaisesRegexp(Exception, "^'(\w+)' no es una variable definida$", norma.traduce, **{'codigo': ['set',200,'var2']})
+        self.assertRaisesRegexp(Exception, "^La direccion '(\d+)' esta fuera de rango$", norma.traduce, **{'codigo': ['set',200,norma.TAM_MEMORIA + 1000]})
+
+    def test_04_label(self):
+        self.assertListEqual(norma.labels, [])
+        norma.traduce(['label', 'etiq1'])
+        self.assertDictEqual(norma.labels[0],{'name':'etiq1','ip':0})
+        norma.traduce(['1000','1001','1002'])
+        norma.traduce(['label', 'etiq2'])
+        self.assertDictEqual(norma.labels[1],{'name':'etiq2','ip':3})
+
+
+class TestMacros(unittest.TestCase):
+    def setUp(self):
+        reload(norma)
+
+    def test_01_carga(self):
+        norma.loader("test/loadmacro.a")
+        self.assertEqual(len(norma.macros), 3)
+        self.assertDictEqual(norma.macros[0], {'name': 'NOT',
+                                               'args': ['a','r'],
+                                               'body': [['a', 'a', 'r']]})
+        self.assertDictEqual(norma.macros[1], {'name': 'OR',
+                                               'args': ['a','b','r'],
+                                               'body': [['local', 't'], ['a', 'b', 't'], ['NOT', 't', 'r']]})
+        self.assertDictEqual(norma.macros[2], {'name': 'SET',
+                                               'args': ['a','b','r'],
+                                               'body': [['local', 't'], ['set', 'a', 't'], ['a', 'b', 'r']]})
+
+    def test_02_exceptions(self):
+        self.assertRaisesRegexp(Exception, "No se encontro 'endm' para la macro (\w+)", norma.loader, **{'nomarchivo': 'test/noendm.a'})
+
+        self.assertRaisesRegexp(Exception, "La macro (\w+) se llama a si misma", norma.loader, **{'nomarchivo': 'test/autorefmacro.a'})
+
+        self.assertRaisesRegexp(Exception, "Macro '(\w+)' ya estaba previamente definida", norma.loader, **{'nomarchivo': 'test/redefmacro.a'})
+
+        self.assertRaisesRegexp(Exception, "El nombre '(\w+)' para la macro es una palabra reservada", norma.loader, **{'nomarchivo': 'test/reserved_label.a'})
+        self.assertRaisesRegexp(Exception, "El nombre '(\w+)' para la macro es una palabra reservada", norma.loader, **{'nomarchivo': 'test/reserved_local.a'})
+        self.assertRaisesRegexp(Exception, "El nombre '(\w+)' para la macro es una palabra reservada", norma.loader, **{'nomarchivo': 'test/reserved_set.a'})
+        self.assertRaisesRegexp(Exception, "El nombre '(\w+)' para la macro es una palabra reservada", norma.loader, **{'nomarchivo': 'test/reserved_macro.a'})
+        self.assertRaisesRegexp(Exception, "El nombre '(\w+)' para la macro es una palabra reservada", norma.loader, **{'nomarchivo': 'test/reserved_endm.a'})
+
+        self.assertRaisesRegexp(Exception, "No puedes definir una macro dentro de otra macro", norma.loader, **{'nomarchivo': 'test/inceptmacro.a'})
+        
+    def test_03_traduce01(self):
+        norma.mem[1000] = 200
+        norma.loader("test/simplemacro.a")
+        self.assertListEqual(norma.mem[0:3], [1000, 1000, 1001])
+        
+    def test_04_traduce02(self):
+        norma.loader("test/movmacro.a")
+        self.assertListEqual(norma.mem[0:3], [1000, 1000, 1001]) # NOT 1000, 1001
+        self.assertListEqual(norma.mem[3:6], [1001, 1001, norma.DATA]) # MOV 1001, 1002 -> OR 1001, 1001, 1002 (1er linea): 1001, 1001, t
+        self.assertListEqual(norma.mem[6:9], [norma.DATA, norma.DATA, norma.OUT]) # MOV 1001, OUT -> OR 1001, 1001, OUT (2da linea): NOT t, OUT -> t, t, OUT
+        self.assertListEqual(norma.mem[9:12], [1003, 1003, norma.IP]) # NOT 1003, IP
+        self.assertListEqual(norma.mem[12:norma.DATA], [0]*(norma.DATA-12))
+        self.assertListEqual(norma.mem[norma.DATA:norma.DATA + 1], [0])
+        self.assertListEqual(norma.mem[norma.DATA + 1:norma.OUT], [0]*(0xFFC))
+
+    def test_05_traduce03(self):
+        norma.loader("test/branchmacro.a")
+        # falta un programa que haga algo y probar que la memoria lo contenga bien
+
+    def test_06_traducenot(self):
+        norma.loader("test/not.a")
+        self.assertListEqual(norma.mem[norma.DATA : norma.DATA+1], [0])
+        self.assertListEqual(norma.mem[0:3], [norma.DATA, norma.DATA, norma.OUT])
+        self.assertListEqual(norma.mem[3:6], [1003, 1003, norma.IP])
+        self.assertListEqual(norma.mem[6:norma.DATA], [0]*(norma.DATA-6))
+        self.assertListEqual(norma.mem[norma.DATA + 1:norma.OUT], [0]*(0xFFC))
+
+
+class TestCpu(unittest.TestCase):
+    def setUp(self):
+        reload(norma)
+
+    def test_01_not(self):
+        norma.cpu("test/not.a")
+        self.assertEqual(norma.mem[norma.OUT], 0xFFFF)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestConvierte)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTraduce)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMacros)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCpu)
+    unittest.TextTestRunner(verbosity=2).run(suite)
